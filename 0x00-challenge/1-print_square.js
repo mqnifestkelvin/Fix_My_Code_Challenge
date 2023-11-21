601561b44feb57e7120c87600ffd9b1048ce51de
mqnifestkelvin <mqnifestkelvin@gmail.com>
2023-11-21 11:36:33 +0100

    fixed code

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -16,6 +16,11 @@ if (process.argv.length <= 2) {
 
 size = parseInt(process.argv[2])
 
+if (isNaN(size)) {
+    process.exit(1);
+
+}
+
 for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
         process.stdout.write("#");
